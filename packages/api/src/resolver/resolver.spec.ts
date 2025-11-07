7142f87c7b9f2f927dec857c972a79767455cd96
Aníbal Jorquera <ajorquera.cornejo@gmail.com>
2025-11-07 15:20:54 -0300

    feat: add resolver test

---
+++ packages/api/src/resolver/resolver.spec.ts
@@ -0,0 +1,56 @@
+import 'cdktf/lib/testing/adapters/jest';
+import { enableBuildEnvVariable } from '@alicanto/common';
+import { type AppStack, setupTestingStackWithModule } from '@alicanto/resolver';
+import { Testing } from 'cdktf';
+import { ApiResolver } from './resolver';
+import { RestApi } from './rest-api/rest-api';
+
+describe('Api Resolver', () => {
+  enableBuildEnvVariable();
+  it('should create a new rest api with default properties in before create hook', async () => {
+    const { stack, module } = setupTestingStackWithModule();
+
+    const resolver = new ApiResolver();
+
+    await resolver.beforeCreate(module as AppStack);
+    const synthesized = Testing.synth(stack);
+
+    expect(synthesized).toHaveResourceWithProperties(RestApi, {
+      name: 'test-general',
+    });
+  });
+
+  it('should create a new rest api in before create hook', async () => {
+    const { stack, module } = setupTestingStackWithModule();
+
+    const resolver = new ApiResolver({
+      restApi: {
+        name: 'testing',
+      },
+    });
+
+    await resolver.beforeCreate(module as AppStack);
+    const synthesized = Testing.synth(stack);
+
+    expect(synthesized).toHaveResourceWithProperties(RestApi, {
+      name: 'testing',
+    });
+  });
+
+  it('should create a new rest api in before create hook', async () => {
+    const { stack, module } = setupTestingStackWithModule();
+
+    const resolver = new ApiResolver({
+      restApi: {
+        name: 'testing',
+      },
+    });
+
+    await resolver.beforeCreate(module as AppStack);
+    const synthesized = Testing.synth(stack);
+
+    expect(synthesized).toHaveResourceWithProperties(RestApi, {
+      name: 'testing',
+    });
+  });
+});
